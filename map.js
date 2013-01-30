f437d35eab79259e415cb4f3cee7e7d3aefb0029
Geza Kovacs <gkovacs@mit.edu>
2013-01-30 04:58:05 -0500

    added link to add google calendar events

--- map.js
+++ map.js
@@ -44,7 +44,7 @@ function calendarEntered() {
   $('#scrollwrap').hide()
   $('#calendar').weekCalendar('resize')
   $('#addToGoogleCalendar').show()
-  $('#addToGoogleCalendarLink').attr('href', encodeURI('http://www.google.com/calendar/render?cid=http://psetparty.xvm.mit.edu:3333/exportcal?username=' + email))
+  $('#addToGoogleCalendarLink').attr('href', encodeURI('http://www.google.com/calendar/render?cid=http://psetparty.xvm.mit.edu:3333/exportcal?username=' + email + '&k=' + Math.floor(Math.random()*100000)))
 }
 
 var mapSettingCenterThisRun = true
